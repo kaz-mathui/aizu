275050af9ef15f8111d323fa20ce7ae6a6321371
kaz-mathui <kazmat.hui@gmail.com>
2021-01-26 08:43:33 +0900

    先頭の文字を一番後ろに移動する（文字列長さ分だけ繰り返す）

--- 32.py
+++ 32.py
@@ -1,6 +1,23 @@
-input_str = input()
-judge_str = input()
-if judge_str in input_str+input_str:
-    print("Yes")
+# input_str = input()
+# judge_str = input()
+# if judge_str in input_str+input_str:
+#     print("Yes")
+# else:
+#     print("No")
+
+# coding: utf-8
+
+s = input()
+p = input()
+flg = False
+
+for i in range(len(s)):
+    if p in s:
+        flg = True
+        break
+    s = s[1:] + s[:1]
+
+if flg:
+    print('Yes')
 else:
-    print("No")
+    print('No')
