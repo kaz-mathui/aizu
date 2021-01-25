961b0e3a324a420f02cfc73f3535b4d72d5cace4
kaz-mathui <kazmat.hui@gmail.com>
2021-01-25 22:03:21 +0900

    まじで総当たりやな

---
+++ 26.py
@@ -0,0 +1,15 @@
+result_list = []
+while True:
+    n,x = map(int, input().split())
+    if n == 0 and x ==0:
+        break
+    count = 0
+    for i in range(1,n+1):
+        for j in range(i+1,n+1):
+            for k in range(j+1,n+1):
+                if i+j+k == x:
+                    count += 1
+                    # print(i,j,k)
+    result_list.append(count)
+for i in result_list:
+    print(i)
